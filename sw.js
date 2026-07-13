65f7616b74afc5729c8613faaca193eb8671e25d
zoseldnjs123-debug <zoseldnjs123@gmail.com>
2026-07-13 10:59:58 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "matzip-v3";
+const CACHE = "matzip-v4";
 const SHELL = ["./", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png"];
 
 self.addEventListener("install", e => {
